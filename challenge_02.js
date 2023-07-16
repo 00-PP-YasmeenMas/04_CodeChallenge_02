3c5b576b2cb7c78163bab91a6d471b7a6dbf2bea
YasmeenMas <yasmeenmas@gmx.de>
2023-07-16 21:03:16 +0200

    Variante 2

--- challenge_02.js
+++ challenge_02.js
@@ -21,12 +21,32 @@ let age = parseInt(prompt("Bitte dein Alter eingeben!"));
 
 /*********** Variante 1  ****************/
 
-if (age > 0 && age < 6) {
-  console.log(firstName + " trinkt Milch.");
-} else if (age > 5 && age < 13) {
-  console.log(firstName + " trinkt Saft.");
-} else if (age > 12 && age < 18) {
-  console.log(firstName + " trinkt Cole.");
-} else {
-  console.log(firstName + " trinkt Wein.");
+// if (age > 0 && age < 6) {
+//   console.log(firstName + " trinkt Milch.");
+// } else if (age > 5 && age < 13) {
+//   console.log(firstName + " trinkt Saft.");
+// } else if (age > 12 && age < 18) {
+//   console.log(firstName + " trinkt Cole.");
+// } else {
+//   console.log(firstName + " trinkt Wein.");
+// }
+
+/*********** Variante 2  ****************/
+const cond = true;
+switch (cond) {
+  case age > 0 && age < 6:
+    console.log(firstName + " trinkt Milch.");
+    break;
+  case 5 < age && age < 13:
+    console.log(firstName + " trinkt Saft.");
+    break;
+  case 12 < age && age < 18:
+    console.log(firstName + " trinkt Cole.");
+    break;
+  case age >= 18:
+    console.log(firstName + " trinkt Wein.");
+    break;
+  default:
+    console.log(firstName + " trinkt etwas anderes!");
+    break;
 }
